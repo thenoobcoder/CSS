d411659d92a833275811d14a2d26f8e71aef90f9
thenoobcoder <rahulsharma001f@gmail.com>
2014-12-21 02:08:42 +0530

    Digital Clock
    Now Shows correct time

--- DigitalClock/clock.js
+++ DigitalClock/clock.js
@@ -3,17 +3,30 @@ $(document).ready(function()
 
     function displayDate()
     {
+          var days = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
+
           var currentTime = new Date()  ;
+
+          var date = currentTime.getDay();
           var hours = currentTime.getHours();
           var minutes = currentTime.getMinutes();
           var seconds = currentTime.getSeconds();
 
+          var meridiem = "AM";  
+        
+          if (hours > 12) 
+          {
+              hours = hours - 12; 
+              meridiem = "PM"; 
+          }
+
           //alert("DateTime: "+hours+":"+minutes+":"+seconds);
           var clockDiv = document.getElementById("digclock");
-          clockDiv.innerHTML = hours + ":" + minutes + ":" + seconds;
+          clockDiv.innerHTML = days[date]+"\n"+hours + ":" + minutes + ":" + seconds + "  "+meridiem;
     }
 
     displayDate();
 
+    setInterval(displayDate, 1000);
 
 });
